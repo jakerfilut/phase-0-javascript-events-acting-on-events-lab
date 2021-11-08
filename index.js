6737f538b2c099acef2745bf1322d25c1e486056
Jake <jakerfilut@gmail.com>
2021-11-08 13:32:18 -0700

    commit tree 399408cf1956bd1df99add4addf5066d04320429

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+});
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 10}px`;
+    }
+}
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const right = parseInt(leftNumbers, 10);
+
+    if (right < 360) {
+        dodger.style.left = `${right + 10}px`;
+    }
+}
